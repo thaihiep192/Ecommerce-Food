e351113f2ff17696100ae521bd33026ba2e82662
thaihiep192 <92443001+thaihiep192@users.noreply.github.com>
2023-09-29 11:19:49 +0700

    Initial commit

---
+++ Library/src/main/java/com/example/library/dto/AdminDto.java
@@ -0,0 +1,20 @@
+package com.example.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Size;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class AdminDto {
+
+    @Size(min = 3, max = 15, message = "Invalid first name (3-15 characters)!")
+    private String firstName;
+    @Size(min = 3, max = 15, message = "Invalid last name (3-15 characters)!")
+    private String lastName;
+    private String username;
+    @Size(min = 8, max = 15, message = "Invalid password (8-15 characters)!")
+    private String password;
+    private String repeatPassword;
+}
---
+++ Library/src/main/java/com/example/library/service/impl/AdminServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.library.service.impl;
+
+import com.example.library.dto.AdminDto;
+import com.example.library.model.Admin;
+import com.example.library.repository.AdminRepository;
+import com.example.library.repository.RoleRepository;
+import com.example.library.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Admin findByUsername(String username) {
+        return adminRepository.findByUserName(username);
+    }
+
+    @Override
+    public Admin save(AdminDto adminDto) {
+        Admin admin = new Admin();
+        admin.setFirstName(adminDto.getFirstName());
+        admin.setLastName(adminDto.getLastName());
+        admin.setUserName(adminDto.getUsername());
+        admin.setPassword(passwordEncoder.encode(adminDto.getPassword()));
+        admin.setRoles(Arrays.asList(roleRepository.findByName("ADMIN")));
+        return adminRepository.save(admin);
+    }
+}
